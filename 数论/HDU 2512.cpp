510b608731b1cf7993fa7870f6f92f11ae504eee
StarPolygen <34528868+StarPolygen@users.noreply.github.com>
2019-05-10 01:25:34 +0800

    2019/5/9

---
+++ 数论/HDU 2512.cpp
@@ -0,0 +1,42 @@
+//HDU 2512
+//集合划分 ——贝尔数
+#include<bits/stdc++.h>
+#define ll long long
+#define MAXN 2010
+#define MOD 1000
+#define TYPE long long
+using namespace std;
+
+
+//贝尔数 
+TYPE bell[MAXN];
+TYPE temp[MAXN];
+void init_bell()
+{
+    bell[0] = bell[1] = temp[0] = 1;
+    for (ll i = 2; i < MAXN; i++)
+    {
+        temp[i - 1] = bell[i - 1];
+        for (ll j = i - 2; j >= 0; j--)
+        {
+#ifdef MOD
+            temp[j] = (temp[j] + temp[j + 1]) % MOD;
+#else
+            temp[j] = temp[j] + temp[j + 1];
+#endif
+        }
+        bell[i] = temp[0];
+    }
+}
+
+int main() {
+    init_bell();
+    int n,x;
+    scanf("%d",&n);
+    while(n--){
+        scanf("%d",&x);
+        printf("%lld\n",bell[x]);
+    }
+
+    return 0;
+}
