337deca11b6696579f7e6bee99ad4598b79e913e
StarPolygen <34528868+StarPolygen@users.noreply.github.com>
2019-06-20 21:01:50 +0800

    2019/6/20

---
+++ 比赛/Comet OJ - Contest #5/CometOJE.cpp
@@ -0,0 +1,108 @@
+//NTT模板，该模板内NNT直接对原始数组操作，因此原始数组大小为maxl=maxn<<2
+//注意再次使用时的备份，因为测算出的NTT结果将会保存在第一个原始数组f内，并将原始数据覆盖掉
+//以及如果要将NTT结果（此时在f内）作为下一次ntt的原始数组使用，注意将n之后的内容清空
+//注意原始数组的初始化
+#include<bits/stdc++.h>
+#define maxn 200020
+using namespace std;
+typedef long long ll;
+const ll mod = 998244353;
+const ll _g = 3;
+
+
+//前置模板
+ll mul(ll u, ll v, ll p) {
+    return (u * v - ll((long double) u * v / p) * p + p) % p;
+}
+ll bin(ll x, ll n,ll p) {
+    ll ret = p != 1;
+    for (x %= p; n; n >>= 1, x = mul(x,x,p))
+        if (n & 1) ret = mul(ret,x,p);
+    return ret;
+}
+inline ll get_inv(ll x, ll p) { return bin(x, p - 2, p); }
+ll fac[maxn+10],invf[maxn+10];
+void fact() {
+    fac[0]=1;
+    for(int i=1;i<=maxn;i++) {
+        fac[i]=(fac[i-1]*i) % mod;  //阶乘取余打表
+    }
+    //切记,求阶乘逆元时maxn最大值为mod-1，因为求逆元的数（此处为n!）要和mod互质才存在逆元。
+    invf[maxn] = bin(fac[maxn], mod-2 , mod);  //最大阶乘逆元
+    for(int i=maxn-1;i>=0;i--) {
+        invf[i]=(invf[i+1]*(i+1))%mod;  //递推阶乘逆元
+    }
+}
+
+
+
+//NTT
+const int maxl = maxn<<2; 
+int rev[maxl];
+void NTT(int *a,int len,int f){//此处的len是已经对齐的，对齐工作在mul里完成
+	for(int i=0;i<len;++i)if(i<rev[i])swap(a[i],a[rev[i]]);
+	for(int i=1;i<len;i<<=1){
+		int w1=bin(_g,((mod-1)/(i<<1)*f+mod-1)%(mod-1),mod);
+		for(int j=0;j<len;j+=(i<<1)){
+			int w=1;
+			for(int k=0;k<i;++k,w=1ll*w*w1%mod){
+				int x1=a[j+k],x2=1ll*w*a[j+i+k]%mod;
+				a[j+k]=(x1+x2)%mod;
+				a[j+i+k]=(x1+mod-x2)%mod;
+			}
+		}
+	}
+	if(!(~f)){
+		int invlen=get_inv(len,mod);
+		for(int i=0;i<len;++i)a[i]=1ll*a[i]*invlen%mod;
+	}
+}
+void mul(int *a,int *b,int n){//此处的参数n为原始数组的有用长度之和
+	int len=1,bit=0;
+	while(len<=n)len<<=1,++bit;
+	for(int i=0;i<len;++i)rev[i]=(rev[i>>1]>>1)|((i&1)<<(bit-1));
+	NTT(a,len,1),NTT(b,len,1);
+	for(int i=0;i<len;++i)a[i]=1ll*a[i]*b[i]%mod;
+	NTT(a,len,-1);
+}
+
+
+
+int n,a,b;
+//原始数组要求长度也为maxl
+int f[maxl],g[maxl],z[maxl];
+int main() {
+    fact();
+    scanf("%d%d%d",&n,&a,&b);
+    int p = 1ll*a*get_inv(b,mod)%mod;
+    int q = (1-p+mod)%mod; 
+    //计算i个分身时第一分身最后淘汰概率 存储于f[i]内    
+        //NTT
+        memset(f,0,sizeof(f));
+        memset(g,0,sizeof(g));
+        for(int i=0;i<=n;++i){
+            if(i<n)  f[i]=1ll*bin(mod-1,i,mod)*invf[i]%mod*get_inv((1-bin(q,i+1,mod)+mod)%mod,mod)%mod;
+            if(i)    g[i]=invf[i-1];
+        }
+        mul(f,g,maxn<<1);
+        for(int i=1;i<=n;i++){
+            f[i] = 1ll*f[i] *p % mod *fac[i-1] % mod;
+        }
+        for(int i=n+1;i<maxl;i++) f[i]=0;
+
+    //计算i个分身时，其他分身最后淘汰的概率，可以由第一分身最后淘汰概率推得
+        //NTT
+        memset(g,0,sizeof(g));
+        for(int i=0;i<=n;i++){
+            if(i<n) z[i]=f[n-i]*bin(p,i,mod)%mod*invf[i]%mod;
+            if(i)   g[i]=bin(q,i-1,mod)*invf[i-1]%mod;
+        }
+        mul(z,g,maxn<<1);
+        for(int i=1;i<=n;i++){
+            z[i] = z[i] * fac[i-1] %mod;
+        }
+        for(int i=1;i<=n;i++){
+            printf("%d\n",z[i]);
+        }
+    return 0;
+}
